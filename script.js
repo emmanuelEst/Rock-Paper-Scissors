d4fcacfffb4e482b5730a8e07a649895839b2efa
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-08-27 13:29:51 -0500

    Create computer choice generation
    getComputerChoice requires a random integer from 0-2 in order to generate a random choice in the possibleChoices array

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function playerSelection() {
     choice = prompt("Pick rock, paper, or scissors: "); // stores user input in choice
     return choice.toLowerCase(); // return user input in all lowercase making it case-insensitive
 }
+// generates the choice of the computer
+function getComputerChoice() {
+    let possibleChoices;
+    possibleChoices = ['rock', 'paper', 'scissors']; // create an array containing possible choices
+    let arrayIndex = getRandomInt(0, 2); // create an index to be used on possible choices 
+    return possibleChoices[arrayIndex]; // returns the value located in possibleChoices array
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min); // rounds up min
+    max = Math.floor(max); // rounds down max
+    return Math.floor(Math.random() * (max - min + 1) + min); // generates a number from min - max (inclusive)
+}
