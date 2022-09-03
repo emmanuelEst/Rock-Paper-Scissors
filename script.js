37d1a1a3e3bef11e9729955cbde1ce15944fa955
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-03 17:18:23 -0500

    Remove console logs in playRound Add DOM methods

--- script.js
+++ script.js
@@ -25,34 +25,38 @@ function getRandomInt(min, max) {
 }
 
 function playRound(event) {
+    const resultDiv = document.querySelector('#result');
+    const playerChoiceDiv = document.querySelector('#player-choice');
+    const computerChoiceDiv = document.querySelector('#comp-choice');
     let userChoice = playerSelection(event);
-    let compChoice = getComputerChoice();;
+    let compChoice = getComputerChoice();
+
+    computerChoiceDiv.textContent = `Computer Choice: ${compChoice}`;
+    playerChoiceDiv.textContent = `Player Choice: ${userChoice}`;
     // Tie condition
-    console.log(`Computer Choice: ${compChoice}`);
-    console.log(`Your Choice: ${userChoice}`);
     if (compChoice === userChoice) {
-        return 'It\'s a tie!';
+        resultDiv.textContent = 'It\'s a tie!';
         // Win conditions
     } else if (userChoice === 'rock' && compChoice === 'scissors') {
-        return 'You win! Rock beats scissors.';
+        resultDiv.textContent = 'Result: You win! Rock beats scissors.';
 
     } else if (userChoice === 'paper' && compChoice === 'rock') {
-        return 'You win! Paper beats rock.';
+        resultDiv.textContent = 'Result: You win! Paper beats rock.';
 
     } else if (userChoice === 'scissors' && compChoice === 'paper') {
-        return 'You win! Scissors beats paper.';
+        resultDiv.textContent = 'Result: You win! Scissors beats paper.';
         // Lose conditions
     } else if (compChoice === 'scissors' && userChoice === 'paper') {
-        return 'You lose! Scissors beats paper.';
+        resultDiv.textContent = 'Result: You lose! Scissors beats paper.';
 
     } else if (compChoice === 'rock' && userChoice === 'scissors') {
-        return 'You lose! Rock beats scissors.';
+        resultDiv.textContent = 'Result: You lose! Rock beats scissors.';
 
     } else if (compChoice === 'paper' && userChoice === 'rock') {
-        return 'You lose! Paper beats rock.';
+        resultDiv.textContent = 'Result: You lose! Paper beats rock.';
         // No condition
     } else {
-        return 'No conditions met!'
+        resultDiv.textContent = 'Result: No conditions met!'
     }
 }
 // plays a game of rock paper scissors for 5 rounds
