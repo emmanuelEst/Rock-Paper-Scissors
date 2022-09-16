b34fa7b103d30c82a38c66b23e599fbd978cba7e
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-16 12:08:13 -0500

    Add tie counter improving results feedback

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function getRandomInt(min, max) {
     max = Math.floor(max); // rounds down max
     return Math.floor(Math.random() * (max - min + 1) + min); // generates a number from min - max (inclusive)
 }
+// Counts Ties;
+let tieCounter = 0;
 
 function playRound(event) {
     let userChoice = playerSelection(event); // Passes event to playerSelection and stores the result here
@@ -38,39 +40,51 @@ function playRound(event) {
 
     // Tie condition
     if (compChoice === userChoice) {
-        paperResultDiv.textContent = 'It\'s a tie!';
-
+        if (tieCounter > 0) {
+            tieCounter++;
+            paperResultDiv.textContent = `It's a tie again x${tieCounter - 1}!`
+        }
+        else {
+            paperResultDiv.textContent = 'It\'s a tie!';
+            tieCounter++;
+        }
         // Win conditions
     } else if (userChoice === 'rock' && compChoice === 'scissors') {
         playerScore++;
         updateScore();
         rockResultDiv.textContent = 'You win! Rock beats scissors.';
+        tieCounter = 0;
 
     } else if (userChoice === 'paper' && compChoice === 'rock') {
         playerScore++;
         updateScore();
         paperResultDiv.textContent = 'You win! Paper beats rock.';
+        tieCounter = 0;
 
     } else if (userChoice === 'scissors' && compChoice === 'paper') {
         playerScore++;
         updateScore();
         scissorsResultDiv.textContent = 'You win! Scissors beats paper.';
+        tieCounter = 0;
 
         // Lose conditions
     } else if (compChoice === 'scissors' && userChoice === 'paper') {
         computerScore++;
         updateScore();
         paperResultDiv.textContent = 'You lost! Paper does not beat scissors.'
+        tieCounter = 0;
 
     } else if (compChoice === 'rock' && userChoice === 'scissors') {
         computerScore++;
         updateScore();
         scissorsResultDiv.textContent = 'You lost! Scissors does not beat rock.'
+        tieCounter = 0;
 
     } else if (compChoice === 'paper' && userChoice === 'rock') {
         computerScore++;
         updateScore();
         rockResultDiv.textContent = 'You lost! Rock does not beat paper.'
+        tieCounter = 0;
 
         // No condition
     } else {
